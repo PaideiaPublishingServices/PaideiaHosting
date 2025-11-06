3fb5085a4d9759719b18c30bff8e864bef4817fe
Ppantaleo <patriciopantaleo@hotmail.com>
2025-11-06 10:06:42 -0300

    refactor: Remove suppressHydrationWarning from ThemeProvider component

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,10 +2,6 @@
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props} suppressHydrationWarning>
-      {children}
-    </NextThemesProvider>
-  )
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
